f1762dee77f8e26b95a61c51547149674b92a79d
Milena <132115087+milena-allaway@users.noreply.github.com>
2023-07-29 22:19:55 -0400

    added script for quiz intro, commented out html sample for script content

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,60 +1,80 @@
-var questions = [
-    {
-        question: "question 1",
-        answers: {
-            a: "",
-            b: "",
-            c: "",
-            d: ""
-        },
-        correct: ""
-    },
-    {
-        question: "question 2",
-        answers: {
-            a: "",
-            b: "",
-            c: "",
-            d: ""
-        },
-        correct: ""
-    },
-    {
-        question: "question 3",
-        answers: {
-            a: "",
-            b: "",
-            c: "",
-            d: ""
-        },
-        correct: ""
-    },
-    {
-        question: "question 4",
-        answers: {
-            a: "",
-            b: "",
-            c: "",
-            d: ""
-        },
-        correct: ""
-    },
-    {
-        question: "question 5",
-        answers: {
-            a: "",
-            b: "",
-            c: "",
-            d: ""
-        },
-        correct: ""
-    },
-]
-var score = 0;
+var body = document.body;
+var h1El = document.createElement("h1");
+var infoDiv = document.createElement("div");
+var infoTextDiv = document.createElement("div");
+var btnDiv = document.createElement("div");
+var startBtn = document.createElement("button");
 
-for (var i = 0; i < questions.length; i++) {
-    var userAnswer = //code here
-    if (userAnswer === questions[i].correct) {
-        score++;
-    }
-}
+h1El.textContent = "Coding Quiz Challenge";
+infoTextDiv.textContent = "Answer the following questions related to JavaScript within the time limit. Every wrong answer will deduct 10 seconds from your time. Good luck!";
+startBtn.textContent = "Start Quiz";
+
+body.appendChild(h1El);
+body.appendChild(infoDiv);
+infoDiv.appendChild(infoTextDiv);
+infoTextDiv.appendChild(btnDiv);
+btnDiv.appendChild(startBtn);
+
+h1El.setAttribute("style", "text-align:center; font-weight:bolder;");
+infoTextDiv.setAttribute("style", "font-size:20px; text-align:center;");
+startBtn.setAttribute("style", "margin-top:40px;");
+// var questions = [
+//     {
+//         question: "question 1",
+//         answers: {
+//             a: "",
+//             b: "",
+//             c: "",
+//             d: ""
+//         },
+//         correct: ""
+//     },
+//     {
+//         question: "question 2",
+//         answers: {
+//             a: "",
+//             b: "",
+//             c: "",
+//             d: ""
+//         },
+//         correct: ""
+//     },
+//     {
+//         question: "question 3",
+//         answers: {
+//             a: "",
+//             b: "",
+//             c: "",
+//             d: ""
+//         },
+//         correct: ""
+//     },
+//     {
+//         question: "question 4",
+//         answers: {
+//             a: "",
+//             b: "",
+//             c: "",
+//             d: ""
+//         },
+//         correct: ""
+//     },
+//     {
+//         question: "question 5",
+//         answers: {
+//             a: "",
+//             b: "",
+//             c: "",
+//             d: ""
+//         },
+//         correct: ""
+//     },
+// ]
+// var score = 0;
+
+// for (var i = 0; i < questions.length; i++) {
+//     var userAnswer = //code here
+//     if (userAnswer === questions[i].correct) {
+//         score++;
+//     }
+// }
